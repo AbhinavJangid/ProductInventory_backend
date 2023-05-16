b3602611941334c3f4292da4442a529018d5e7ec
Abhinav <abhinav1905@outlook.com>
2023-05-16 23:11:07 +0530

    backend for product inventory

---
+++ src/test/java/com/apk/productInventory/ProductInventoryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apk.productInventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
